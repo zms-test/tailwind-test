012070df16efb804e75700c630b7871b1152e381
zerone2 <yicho93@naver.com>
2020-12-02 16:27:04 +0900

    [init - App.js]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { MyHistoryPage } from 'pages'
+
+const browserHistory = createBrowserHistory()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter history={browserHistory}>
+      <Switch>
+        <Route exact path={'/'} component={() => <></>} />
+        <Route path={'/my_history'} component={MyHistoryPage} />
+        <Redirect path={'*'} to={'/'} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
